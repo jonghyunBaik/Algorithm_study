6743ea658b347f5a1c5d2231faffbfe980b0004f
jonghyunBaik <pigmammam4@gmail.com>
2023-11-06 10:19:15 +0900

    [add/update] 스터디 문제
    프로그래머스 : 세로 읽기
    파일 구조 수정

---
+++ Algorithm/Problems/세로 읽기.swift
@@ -0,0 +1,19 @@
+//
+//  세로 읽기.swift
+//  Algorithm
+//
+//  Created by jonghyun baik on 11/6/23.
+//
+
+import Foundation
+
+func solution(_ my_string:String, _ m:Int, _ c:Int) -> String {
+    var answer = ""
+    let a : [Character] = Array(my_string)
+    for i in 0..<my_string.count {
+        if i % m == c-1 {
+            answer += String(a[i])
+        }
+    }
+    return answer
+}
--- Algorithm/main.swift
+++ Algorithm/main.swift
@@ -1,2 +1,5 @@
 import Foundation
 
+
+var a = "sdfhjksfhsf"
+
